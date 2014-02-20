f34dcb61c16fc4653b55c557895fe92aefccedd3
githop <githop@gmail.com>
2014-02-20 16:36:40 -0500

    there we go

--- bogclass.rb
+++ bogclass.rb
@@ -1,8 +1,6 @@
 class BoggleBoard
 	def initialize(board)
 		@board = board
-
-
 	end
 
 	def create_word(*coords)
@@ -11,17 +9,16 @@ class BoggleBoard
 	end
 
 	def get_row(row)
-		@board[row].inspect
+		@board[row]
 	end
 
 	def get_col(col)
-		@board.collect {|row| row[col].inspect}
+		@board.collect {|row| row[col]}
 	end
 
 	def get_coord(row, col) 
 		@board[row][col]
 	end
-
 end
 
 
@@ -32,7 +29,7 @@ dice_grid = [["b", "r", "a", "e"],
 
 boggle_board = BoggleBoard.new(dice_grid)
 
-puts boggle_board.create_word([1,2],[1,1],[2,1],[3,2])
-puts boggle_board.get_row(1)
-puts boggle_board.get_col(1)
-puts boggle_board.get_coord(3, 2)
+puts boggle_board.create_word([1,2],[1,1],[2,1],[3,2]) == "dock"
+puts boggle_board.get_row(1) == ["i", "o", "d", "t"]
+puts boggle_board.get_col(1) == ["r","o","c","a"]
+puts boggle_board.get_coord(3, 2) == "k"
