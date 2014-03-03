9b54abe42e0d45673797f6681472569b3a6656f0
githop <githop@gmail.com>
2014-03-03 16:07:53 -0500

    Update isFib.rb

--- isFib.rb
+++ isFib.rb
@@ -9,14 +9,8 @@ def is_fibonacci?(i, a = 0, b = 1)
 end
 
 
-puts is_fib?(5)
+
 puts is_fib?(927372692193078999171)
-puts Math.sqrt(5*(927372692193078999171**2) + 4) % 1 == 0
-puts (4300100551127196232485157882924375093436201) % 1 
-=======
-end
-
-
 is_fib?(5)
 is_fib?(4)
->>>>>>> ac5dc6bb5d9df5ae0bcf988e85a5d4c6b23fdcff
+
