2a701231e3d86cb68309e2fafa03a89c4a4c41fd
githop <tom@githop.com>
2014-03-19 12:14:48 -0400

    exercise 5 done

--- assessment.rb
+++ assessment.rb
@@ -44,9 +44,22 @@ puts calculate_product_odd([2,10]) == 0
 
 
 # EXERCISE 5
+#5000500 = sum 10k 
+def find_missing_number(str)
+  str = str.scan(/\d{1,}/).collect! { |x| x.to_i } #get rid of commas 
+  p = str.collect! { |x| x.to_i }
+  sum = p.inject { |x, y| x += y }
+  control = 50005000
+  return control - sum   
+end
 
-#<Your code here>
+string_missing_7 = (1..10000).reject { |x| x == 7 }.join(", ") 
+string_missing_4567 = (1..10000).reject { |x| x == 4567 }.join(", ")
+string_missing_9999 = (1..10000).reject { |x| x == 9999 }.join(", ") 
 
+puts find_missing_number(string_missing_7) == 7 
+puts find_missing_number(string_missing_4567) == 4567
+puts find_missing_number(string_missing_9999) == 9999
 
 
 
