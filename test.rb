372fdba4d0d852a2f85b084a16af07edd222eac7
githop <tom@githop.com>
2014-03-20 20:05:58 -0400

    brb

--- test.rb
+++ test.rb
@@ -1,15 +1,9 @@
-#5000500 = sum 10k 
-def find_missing_number(str)
-  str = str.scan(/\d{1,}/).collect! { |x| x.to_i } #get rid of commas 
-  sum = str.inject { |x, y| x += y }
-  control = 50005000
-  return control - sum   
+def valid_string?(str)
+  return true if str.match "(\\[.+\\])" || "|(\\(\\))" || "|({})"  
 end
 
-string_missing_7 = (1..10000).reject { |x| x == 7 }.join(", ") 
-string_missing_4567 = (1..10000).reject { |x| x == 4567 }.join(", ")
-string_missing_9999 = (1..10000).reject { |x| x == 9999 }.join(", ") 
 
-puts find_missing_number(string_missing_7) == 7 
-puts find_missing_number(string_missing_4567) == 4567
-puts find_missing_number(string_missing_9999) == 9999
+puts valid_string?("[ ]") == true                 # returns true
+puts valid_string?("[  ") == false                # returns false
+puts valid_string?("[ ( text ) {} ]") == true      # returns true
+puts valid_string?("[ ( text { ) } ]") == true    # returns false
