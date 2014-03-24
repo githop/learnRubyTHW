76c2299fe7a57529709d2861fa98e0e832ca86ec
githop <tom@githop.com>
2014-03-23 23:18:39 -0400

    prep

--- assessment.rb
+++ assessment.rb
@@ -11,33 +11,37 @@ p last_name  == "Hopkins"
 # EXERCISE 2
 
 def calculate_product(num)
-  y = 0
-  num.each { |x|   y += x  }
+  y = 1
+  num.each { |x| y *= x  }
   y
 end
 
-puts calculate_product([1,2,3]) == 6 
-puts calculate_product([0,-1,-10]) == -11 
-puts calculate_product([1,-1,-10]) == -10
-puts calculate_product([]) == 0 
-
+puts "\nex2"
+puts calculate_product([1,2,3]) == 6 # this test puts 'true' if calculate_product([1,2,3]) returns the correct value of 6
+puts calculate_product([0,-1,-10]) == 0
+puts calculate_product([1,-1,-10]) == 10 
+puts calculate_product([]) == nil 
+#if I were to use an enumerable, it would likely be inject; e.g., 
+# num.inject(:*) 
 
 # EXERCISE 3
 def calculate_product_odd(arr)
-  y = 0
-  arr.each { |x| x.odd? ? y += x : x }
+  product = []
+  arr.each { |x| x.odd? ? product << x  : x }
+  y = 1
+  product.each { |x| y *= x }
   y
 end
-
-puts calculate_product_odd([1,2,3]) == 4      # returns true, because 2 is even
-puts calculate_product_odd([0,-1,-10]) == -1  # returns true, because 0 and -10 are even
-puts calculate_product_odd([1,2,3,4,5]) == 9 # returns true, because 4 and 2 are even
-puts calculate_product_odd([2,10]) == 0 
+puts "\nex3"
+puts calculate_product_odd([1,2,3]) == 3      # returns true, because 2 is even
+puts calculate_product_odd([0,-1,-10]) == -1   # returns true, because 0 and -10 are even
+puts calculate_product_odd([1,2,3,4,5]) == 15  # returns true, because 4 and 2 are even
+puts calculate_product_odd([2,10]) == nil
 
 # EXERCISE 4
 
 =begin
-:first and :last are symbols that act as hash keys for an undefined hash. format_name takes as input ahash and returns the values formatted as last name, first name. display_name prints the output of format_name. It's also passing the same argument. 
+:first and :last are symbols that act as hash keys for an undefined hash. format_name takes as input a hash and returns the values formatted as last name, first name. display_name prints the output of format_name. It's also passing the same argument. 
 =end
 
 
@@ -47,15 +51,16 @@ puts calculate_product_odd([2,10]) == 0
 #5000500 = sum 10k 
 def find_missing_number(str)
   str = str.scan(/\d{1,}/).collect! { |x| x.to_i } #get rid of commas 
-  sum = str.inject { |x, y| x += y }
+  sum = str.inject { |x, y| x += y } # sum array 
   control = 50005000
-  return control - sum   
+  return control - sum   # diff = number picked. 
 end
 
 string_missing_7 = (1..10000).reject { |x| x == 7 }.join(", ") 
 string_missing_4567 = (1..10000).reject { |x| x == 4567 }.join(", ")
 string_missing_9999 = (1..10000).reject { |x| x == 9999 }.join(", ") 
 
+puts "\nex5"
 puts find_missing_number(string_missing_7) == 7 
 puts find_missing_number(string_missing_4567) == 4567
 puts find_missing_number(string_missing_9999) == 9999
@@ -67,7 +72,7 @@ def valid_string?(str)
     return true if str.match "(\\[.+\\])" || "|(\\(\\))" || "|({})"  
 end
 
-
+puts "\nex6"
 puts valid_string?("[ ]") == true                 # returns true
 puts valid_string?("[  ") == false                # returns false
 puts valid_string?("[ ( text ) {} ]") == true      # returns true
@@ -87,27 +92,53 @@ puts valid_string?("[ ( text { ) } ]") == true    # returns false
 =begin
 
   Reverse Polish Notation Calculator
+    class RPNCalculator
+      def evaluate(expression)
+	expression = expression.split
+	operands = []
+	evaluation = []
 
-    <paste code sample>
+	expression.each do |x|
+	  case x
+	    when /\d/
+	      evaluation.push(x.to_f)
+            when "-", "/", "*", "+", "**"
+	      operands = evaluation.pop(2)
+              evaluation.push(operands[0].send(x, operands[1]))
+	    end
+	end
+	return evaluation.pop
+     end
+  end    
 
-    <Your answer here>
-
-
-
+<Your answer here> 
+     There are some other one's like Yim's that are more concise and take advantage of ruby's short hand, 
+     however I like this one because it matches up well with the postfix algorithm..http://en.wikipedia.org/wiki/Reverse_Polish_notation      
 
   Reverse Words
+   def reverse_words(str)
+     words = str.split.each { |word| word.reverse! }
+     reversed = words.join(" ")
+    end
 
-    <paste code sample>
-
-    <Your answer here>
+    <Your answer here> 
+    The way the variables are named in this one makes it very easy to follow along with the flow of the code.
+    first prep the words to be reversed, then combine them after so. 
 
 
 
 
   Fibonacci Number
 
-    <paste code sample>
+    def is_fibonacci?(i, zero=0, one=1)
+     return true if one == i
+      one > i ? false : is_fibonacci?(i, one, zero + one)
+     end
 
     <Your answer here>
+    I chose this one for 3 reasons:
+    1. It's recursive, which was the focus of the exercise
+    2. It takes advantage of the ruby shortand
+    3. the variables are named clearly 
 
 =end
--- test.rb
+++ test.rb
@@ -1,9 +1,69 @@
-def valid_string?(str)
-  return true if str.match "(\\[.+\\])" || "|(\\(\\))" || "|({})"  
+class House
+  def initialize(current_temp, minimum_temp, maximum_temp) 
+    @current_temp = current_temp
+    @minimum_temp = minimum_temp
+    @maximum_temp = maximum_temp
+    @ac_on = false
+    @heater_on = false
+  end
+
+  attr_reader :heater_on, :ac_on, :current_temp
+
+  def toggle_heater
+    if @heater_on == false and @ac_on == false
+      @heater_on = true
+    else
+      @heater_on = false
+    end
+  end
+
+  def toggle_ac
+    if @ac_on == false and @heater_on == false
+      @ac_on = true
+    else
+      @ac_on = false
+    end
+  end
+
+  def update_temp!
+
+    if @current_temp > @maximum_temp
+      if @ac_on == false and @heater_on == true 
+	@ac_on = true
+	@heater_on = false	
+	@current_temp += 2
+      elsif @ac_on == true and @heater_on == false
+	@ac_on = false
+	@heater_on = true
+	@current_temp -= 1
+      end
+    elsif @current_temp < @maximum_temp
+      if @ac_on == true and @heater_on == false
+	@ac_on = false
+	@heater_on = true
+	@current_temp -= 1
+      elsif @ac_on == false and @heater_on == true 
+	@ac_on = true
+	@heater_on = false
+	@current_temp += 2
+      end
+    else
+      @current_temp
+    end
+
+
+  end
 end
 
+my_house = House.new(75, 68, 74)
 
-puts valid_string?("[ ]") == true                 # returns true
-puts valid_string?("[  ") == false                # returns false
-puts valid_string?("[ ( text ) {} ]") == true      # returns true
-puts valid_string?("[ ( text { ) } ]") == true    # returns false
+def calculate_product_odd(arr)
+  y = []
+  arr.each { |x| x.odd? ? y << x : x }
+  print y.each { }
+end
+
+puts calculate_product_odd([1,2,3])    # returns true, because 2 is even
+puts calculate_product_odd([0,-1,-10])  # returns true, because 0 and -10 are even
+puts calculate_product_odd([1,2,3,4,5]) # returns true, because 4 and 2 are even
+puts calculate_product_odd([2,10])
