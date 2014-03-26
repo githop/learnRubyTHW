b9c5d40cee1659b22edd989a67fa87da394af2d3
githop <tom@githop.com>
2014-03-26 18:48:57 -0400

    working

--- gradebook.js
+++ gradebook.js
@@ -52,8 +52,26 @@ that the next test has passed.
 
 // __________________________________________
 // Write your code below.
+  function average(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i]; 
+    }
+    return (sum / arr.length); 
+  }
 
-
+  var gradebook = {
+    Joseph: {'testScores': [80,70,70,100] }, 
+    Susan: {'testScores': [85, 80, 90, 90] },
+    William: {'testScores': [75, 70, 80, 75]},
+    Elizabeth: {'testScores': [100, 90, 95, 85]},
+    addScore: function(name, score) {
+     return gradebook.name.testScores.push(score);  
+    },
+    getAverage: function(name) {
+      return average(name.testScores);
+    },   
+  }
 
 
 
