207e957a9fd5862dfcc007febdbf881266bd43bd
githop <tom@githop.com>
2014-03-28 19:14:38 -0400

    js complete

--- js_dom.js
+++ js_dom.js
@@ -5,13 +5,28 @@ window.onload= function(){
 document.getElementsByTagName('body')[0].style.backgroundColor = "pink"
 
 //RELEASE 1:
-  //Add code here to select elements of the DOM 
+//Add code here to select elements of the DOM 
+
+document.getElementById('body').style.backgroundColor = "gray";
+
+document.getElementsByTagName('body')[0].style.backgroundColor = "red"; 
+
+document.querySelector("body").style.backgroundColor = "green";
 
 //RELEASE 2: 
   // Add code here to manipulate the DOM.
-
+document.querySelector('h2').style.color = "yellow";
+document.querySelector('p').style.border = '3px solid';
+document.querySelector('p').style.borderColor = 'yellow';
+document.querySelector('p').style.padding = "5px";
+document.getElementById('testing').innerHTML = "Testing...Complete!"; 
+document.getElementById('hidden').style.visibility = "hidden"; 
 //RELEASE 3: Event Listener
   // Add the code for the event listener here and replace the console.log code with code that changes the color of some DOM element
+
+document.getElementById('color-switcher').addEventListener('click', function(){
+    document.querySelector('h1').style.color = "yellow"; 
+})
 
 //RELEASE 4 : Experiment on your own
 
