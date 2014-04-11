98359b656bd69bcf2297a02b5cebfc6b1d5e8227
githop <githop@gmail.com>
2014-04-11 16:27:59 -0400

    Update debug_drill.rb

--- debug_drill.rb
+++ debug_drill.rb
@@ -1,36 +1,39 @@
 def valid_triangle?(a, b, c)
-  if a != 0 || b != 0 || c != 0
-  if a >= b
-  largest = a
-  sum += b
-  else largest = b
-  sum += a
-  end
-  if c > largest
-  sum += largest
-  largest = c
-  else sum += c
-  end
-  if sum > largest
-  return "true"
-  else return "false"
-  end
-  else return "false"
-  end
-  end
+    sum = 0
+    if a != 0 and b != 0 and c != 0
+    
+      if a >= b
+        largest = a
+        sum += b
+      else 
+        largest = b
+        sum += a
+      end
+    
+      if c > largest
+        sum += largest
+        largest = c
+      else
+        sum += c
+      end
+    
+      if sum > largest
+        return true
+      else 
+        return false
+      end
+    
+    else 
+      return false
+    end
+end
 
-def valid_triangle(a,b,c)
-  if a != 0 || b != 0 || c != 0
-  if a >= b
-  largest = a
-  sum += b
-  else largest = b
-  sum += a
-  end
+puts valid_triangle?(0,0,0) # => false, because a triangle can't have 0-length sides
 
-  if c > largest
-  end
-  end
-  end
+puts valid_triangle?(1,1,1) # => true, an equilateral triangle
 
-valid_triangle?(1,2,3)
+puts valid_triangle?(3,4,5) # => true, a right triangle
+puts valid_triangle?(4,3,5) # => true, the same right triangle
+puts valid_triangle?(5,3,4) # => true, the same right triangle
+
+puts valid_triangle?(10, 10, 100) #=> false, no such triangle exists
